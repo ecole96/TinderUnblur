f69e2c76d924cb9438f7e98b906f5e8ff819e79a
Evan Cole <ecole96@outlook.com>
2023-05-20 08:44:45 -0400

    fixed

--- src/background.js
+++ src/background.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    chrome.webNavigation.onHistoryStateUpdated.addListener( // waits for URL changes
-        function(details) {
-            const currentURL = details.url;
-            const blurredURL = 'https://tinder.com/app/likes-you';
-            if(currentURL === blurredURL) { // message only sent to content script when on the blurred "Likes You" page
-                chrome.tabs.sendMessage(details.tabId, {toUnblur: true}); 
-            }
+document.addEventListener('DOMContentLoaded', () => {
+    // waits for URL changes
+    chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+        const currentURL = details.url;
+        const blurredURL = 'https://tinder.com/app/likes-you';
+        if(currentURL === blurredURL) { // message only sent to content script when on the blurred "Likes You" page
+            chrome.tabs.sendMessage(details.tabId, { toUnblur: true }); 
         }
-    );
+    });
 });
--- src/content.js
+++ src/content.js
@@ -1,28 +1,25 @@
-async function unblur() {
-    let retcode;
+const unblur = async () => {
     try {
         const blurredPhotos = document.querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
-        const unblurredPhotos = await fetch("https://api.gotinder.com/v2/fast-match/teasers", 
-                                        {"headers": {"X-Auth-Token": localStorage.getItem('TinderWeb/APIToken')}})
+        const unblurredPhotos = await fetch("https://api.gotinder.com/v2/fast-match/teasers", {
+                                    'headers': {
+                                        'X-Auth-Token': localStorage.getItem('TinderWeb/APIToken'),
+                                        'platform': 'android'
+                                }})
                                 .then(res => res.json())
                                 .then(res => res.data.results); // grab unblurred images
-        blurredPhotos.forEach((blurred,i) => {
-            const unblurred = unblurredPhotos[i].user.photos[0].url;
+        blurredPhotos.forEach((blurred, i) => {
+            const { user: {_id: userId, photos: [{ id: photoId }]} } = unblurredPhotos[i];
+            const unblurred = `https://preview.gotinder.com/${userId}/original_${photoId}.jpeg`;
             blurred.style.backgroundImage = `url(${unblurred})`; // replace each blurred images with its unblurred counterpart
         });
-        retcode = 0; // success
     }
     catch(err) { // error occurred
-        console.log("Tinder Unblur error:",err);
-        retcode = 1; // failure
+        console.error("Tinder Unblur error:",err);
     }
-    return retcode; 
 }
 
-chrome.runtime.onMessage.addListener( // message is sent from background script to content script when ready to unblur
-    function(message, sender, sendResponse) {
-        setTimeout(() => { // wait 2 seconds for page to load before unblurring
-            unblur();
-        },2000);
-    }
-);
+// message is sent from background script to content script when ready to unblur
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    setTimeout(() => unblur(), 2000);  // wait 2 seconds for page to load before unblurring
+});
